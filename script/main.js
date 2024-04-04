e93826beb8ad4ba9ce0d96fd2e8cce7c9d1341a1
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2024-04-04 01:04:23 -0600

    docs: add comments and organize code

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
+// Creates an HTML element for a card displaying Pokemon Information
+// Each item inputs an index from #1 - #100
 function createCardElement(item, index) {
   const cardNumber = index + 1;
-    return `
+  return `
     <li class="card" >
         <img class="pokemoncard" src=${item.image} alt="">
         <div class="card-content">
@@ -18,23 +20,26 @@ function createCardElement(item, index) {
   `;
 }
 
+// Creates HTML elements for a batch of cards.
+// It takes an array of data (Pokemon items) as input
 function createCardElements(data) {
   return data.map(createCardElement).join("");
 }
 
+// Fetches a list of 150 Pokemons from an API
 async function fetch150PokemonList() {
   try {
-
     const response = await fetch(
       "https://pokeapi.co/api/v2/pokemon?offset=150&limit=150"
     );
-    const data = await response.json(); 
+    const data = await response.json();
     return data.results;
   } catch (error) {
     console.log(error);
   }
 }
 
+// Fetches details of Pokemon from a given URL
 async function fetchPokemonDetails(url) {
   try {
     const response = await fetch(url);
@@ -46,12 +51,17 @@ async function fetchPokemonDetails(url) {
   }
 }
 
-// Fetch details for Pokemon from 151(start) to 250(end) Which is the Johto region pretty much
+// Fetches details of from start to end Pokemons (Johto Region)
 async function fetch150PokemonDetails(start, end) {
   const detailsList = [];
-  const urls = Array.from({ length: end - start + 1}, (_, i) => `https://pokeapi.co/api/v2/pokemon/${start + i}`);
-  const response = await Promise.all(urls.map(url => fetchPokemonDetails(url)));
-  response.forEach(data => {
+  const urls = Array.from(
+    { length: end - start + 1 },
+    (_, i) => `https://pokeapi.co/api/v2/pokemon/${start + i}`
+  );
+  const response = await Promise.all(
+    urls.map((url) => fetchPokemonDetails(url))
+  );
+  response.forEach((data) => {
     if (data) {
       detailsList.push(data);
     }
@@ -59,17 +69,17 @@ async function fetch150PokemonDetails(start, end) {
   return detailsList;
 }
 
-// Added Lazy Loading Scroll Event for optimizing the overall performance of the website
+// Enhances the rendering of Pokemon Crds
 async function renderOption2Enhanced() {
   const data = await fetch150PokemonDetails(152, 251);
   const batch = data;
   renderCards(batch);
 }
 
-renderOption2Enhanced()
+renderOption2Enhanced();
 
-// HTML elements for the Pokemon Cards
-function renderCards(data){
+// Renders the HTML elements for Pokemon cards (152, 251)
+function renderCards(data) {
   const cards = createCardElements(
     data.map((item) => ({
       image: item.sprites.other["official-artwork"].front_default,
@@ -80,45 +90,39 @@ function renderCards(data){
   document.getElementById("option-2-enhanced-results").innerHTML = cards;
 }
 
-// If the user scrolls to fast >
-// To prevent multiple simultanoeous loading request for lazy loading
-// This ensures that only one loading request is processed at a time
+// Loads more Pokemon Details when the user scrolls down
 let isLoading = false;
-// Lazy Loading Event > More Cards as the user scrolls
 window.addEventListener("scroll", () => {
-  // if the user is currently not scrolling and they are near the bottom of the page
-  if(!isLoading && window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
+  if (
+    !isLoading &&
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 500
+  ) {
     isLoading = true;
-    loadMore(); // Load more cards
+    loadMore();
   }
 });
 
-// Function to load more cards
-// This ensures a smooth and controlled process of loading additional content as the user scrolls through the page
-async function loadMore(){
+async function loadMore() {
   const data = await fetch150PokemonDetails();
   isLoading = false;
 }
 
-
+// Handles seach bar events by filtering displayed cards based on what the user inputs
 function searchbarEventHandler() {
-
   let input = document.getElementById("searchbar").value;
   input = input.toLowerCase();
-  //Get all the cards
   const enhancedResults = document.getElementById("option-2-enhanced-results");
   const card = enhancedResults.getElementsByClassName("card");
 
   for (i = 0; i < card.length; i++) {
-
     if (!card[i].innerHTML.toLowerCase().includes(input)) {
       card[i].style.display = "none";
-
     } else {
       card[i].style.display = "block";
     }
   }
 }
 
+// For keyup events on the search bar.
 const searchbar = document.getElementById("searchbar");
 searchbar.addEventListener("keyup", searchbarEventHandler);
